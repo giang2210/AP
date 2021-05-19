f69a565a440c531432754e19c4f2d44ce13b0692
giang <70989443+giang2210@users.noreply.github.com>
2021-05-19 10:34:12 +0700

    ggg

---
+++ lab/labs2/Diamon.cs
@@ -0,0 +1,16 @@
+﻿using T2008m.lab.labs1;
+
+namespace T2008m.lab.labs2
+{
+    public class Diamon : Product
+    {
+        public int cara;
+
+        public bool CheckAuth()
+        {
+            if (cara > 10)
+                return true;
+            return false;
+        }
+    }
+}
