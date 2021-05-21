845cbe3a5a97509fc4b798b95bd79b2c831767fa
giang <70989443+giang2210@users.noreply.github.com>
2021-05-21 08:03:17 +0700

    bbbb

---
+++ lab/labs3/Cart.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using T2008m.lab.labs1;
+
+namespace T2008m.lab.labs3
+{
+    public class Cart
+    {
+        public int id;
+        public string customer, city, country;
+        public double grandTotal = 0;
+        private List<Product> productList = new List<Product>();
+
+        public void AddProduct(Product product)
+        {
+            productList.Add(product);
+            Console.WriteLine("Them thanh cong");
+        }
+
+        public void DeleteProduct()
+        {
+            Console.WriteLine("Danh sach cac san pham:");
+            foreach (var VARIABLE in productList)
+            {
+                Console.WriteLine(VARIABLE.name);
+            }
+
+            Console.Write("Ban muon xoa san pham so:");
+            var del = Convert.ToInt32(Console.ReadLine());
+            productList.RemoveAt(del);
+        }
+
+        public void GrandTotal()
+        {
+            double Phiship = 0;
+            Console.WriteLine("Danh sach cac san pham:");
+            foreach (var VARIABLE in productList)
+            {
+                Console.WriteLine(VARIABLE.name);
+                grandTotal += (double) VARIABLE.price;
+            }
+
+            if (this.country == "VietNam")
+            {
+                if (this.city == "HN" || this.city == "HCM")
+                {
+                    Phiship = 0.01;
+                    grandTotal += (grandTotal * Phiship);
+                    Console.WriteLine("Tong tien la :" + grandTotal);
+                    return;
+                }
+                else
+                {
+                    Phiship = 0.02;
+                    grandTotal += (grandTotal * Phiship);
+                    Console.WriteLine("Tong tien la :" + grandTotal);
+                    return;
+                }
+            }
+
+            Phiship = 0.05;
+            grandTotal += (grandTotal * Phiship);
+            Console.WriteLine("Tong tien la :" + grandTotal);
+        }
+
+    }
+}
