7dec5b0a59133837554925a144911b1f1dbcbac2
giang <70989443+giang2210@users.noreply.github.com>
2021-05-24 08:10:39 +0700

    vvvvvvv

---
+++ lab/labs4/KhachHang.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace T2008m.lab.labs4
+{
+    public abstract class KhachHang
+    {
+        public string MaKh,HoTen,NRHoaDon,DonGia;
+        public int SoLuong;
+        public string MaKh1
+        {
+            get => MaKh;
+            set => MaKh = value;
+        }
+
+        public string HoTen1
+        {
+            get => HoTen;
+            set => HoTen = value;
+        }
+
+        public string NRHoaDon1
+        {
+            get => NRHoaDon;
+            set => NRHoaDon = value;
+        }
+
+        public int SoLuong1
+        {
+            get => SoLuong;
+            set => SoLuong = value;
+        }
+
+        public string DonGia1
+        {
+            get => DonGia;
+            set => DonGia = value;
+        }
+
+        public abstract void NhapXuat();
+        public abstract void HoaDon();
+
+    }
+
+}
---
+++ lab/labs4/KhachHangNN.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace T2008m.lab.labs4
+{
+    public class KhachHangNN : KhachHang
+    {
+        public float ThanhTien;
+
+        public string QuocTich;
+
+        public string QuocTich1
+        {
+            get => QuocTich;
+            set => QuocTich = value;
+        }
+
+        //overide Khach Hang
+        public override void NhapXuat()
+        {
+            Console.WriteLine("Moi ban nhap ten khach hang: ");
+            HoTen1 = Console.ReadLine();
+            Console.WriteLine("Moi ban nhap ten quoc tich: ");
+            QuocTich1 = Console.ReadLine();
+            Console.WriteLine("Moi ban nhap ma khach hang: ");
+            MaKh1 = Console.ReadLine();
+            Console.WriteLine("Moi ban nhap ngay ra hoa do: ");
+            NRHoaDon1 = Console.ReadLine();
+            Console.WriteLine("Moi ban nhap so luong: ");
+            SoLuong1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("THONG TIN KHACH HANG:");
+            Console.WriteLine("Ten: "+HoTen1+", ma khach hang: "+MaKh1+", ngay ra hoa don: "+NRHoaDon1+", so luong: "+SoLuong1);        }
+
+        public override void HoaDon()
+        {
+            ThanhTien = SoLuong1 * 2000;
+            Console.WriteLine("Hoa don cua: "+HoTen1+" la: "+ThanhTien);
+        }
+    }
+}
